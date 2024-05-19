c676361374b0b072bf0e2724d4f99bbaae8f890c
bludnic <victorcpla@gmail.com>
2024-05-19 19:13:50 +0100

    chore(p2p): add allowed list

--- src/helia.ts
+++ src/helia.ts
@@ -38,6 +38,10 @@ export const helia = await createHelia({
     ],
     services: {
       identify: identify()
+    },
+    connectionManager: {
+      maxConnections: 100,
+      allow: getAllowNodesMultiaddrs()
     }
   }
 })
